4dd9611eb0f9cbd9c2d53c543d3e63f7ec5e45b6
karol <karolinaferreira518@gmail.com>
2022-05-25 21:44:12 -0300

    Autenticação - sem sessão

---
+++ php/autenticacao.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once("conexaoBanco.php");
+
+$email=$_POST['email'];
+$senha=$_POST['senha'];
+//função md5 () criptografia a senha no algoritmo MD5
+$senhaMD5=md5($senha);
+
+$comando="SELECT * FROM usuarios WHERE email='".$email."' AND senha ='".$senhaMD5."'";
+
+$resultado=mysqli_query($conexao,$comando);
+
+$linhas=mysqli_num_rows($resultado);
+
+if($linhas==0){
+    header("Location: ../index.php?retorno=1");
+}else{
+    $usuario=mysqli_fetch_assoc($resultado);
+    if($usuario['nivel']=='1'){
+        header("Location: principalSecretaria.php");
+    }else{
+        header("Location: principalExecutivo.php");
+
+    }
+}
+
+
+
+?>
