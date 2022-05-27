fd0014104a2fba54d1b1d4f35104e34e27535831
karol <karolinaferreira518@gmail.com>
2022-05-26 22:18:34 -0300

    teste

--- php/cadastrarRelacao.php
+++ php/cadastrarRelacao.php
@@ -4,9 +4,9 @@ require_once("conexaoBanco.php");
 
 $descricao=$_POST['descricao'];
 
-$comando="INSERT INTO relacoes (descricao) VALUES('".$descricao"')";
+$comando="INSERT INTO relacoes (descricao) VALUES ('".$descricao."')";
 
-$resultado=mysqli_query($conexao,$comando);
+$resultado=mysqli_query($conexao, $comando);
 
 if($resultado==true){
     header("Location: relacaoForm.php?retorno=1");
