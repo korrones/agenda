9c5833115f09e27c28ba8d1195a58763a63e2291
karol <karolinaferreira518@gmail.com>
2022-05-30 21:33:07 -0300

    cadastro pessoa finalizado
    commit pt2

--- php/cadastrarPessoa.php
+++ php/cadastrarPessoa.php
@@ -9,12 +9,22 @@ $foto=$_FILES['foto']['name'];
 $idRelacao=$_POST['idRelacao'];
 
 $extensao= strtolower(substr($foto, -4));
-echo "extensao do arquivo:".$extensao;
 
-$novonomeFoto = date("Y.m.d-H.i.s") . $extensao;
+
+$novoNomeFoto = date("Y.m.d-H.i.s") . $extensao;
 $pasta="../fotos/";
 
-move_uploaded_file($_FILES['foto']['name'], $pasta, $novoNomeFoto);
+move_uploaded_file($_FILES['foto']['tmp_name'], $pasta.$novoNomeFoto);
+
+$comando= "INSERT INTO pessoas (nome, sobrenome, email, foto, relacoes_idRelacao) VALUES ('".$nome."','".$sobrenome."','".$email."','".$novoNomeFoto."', $idRelacao)";
+
+echo $comando;
+
+$resultado= mysqli_query($conexao, $comando);
+if($resultado){
+    header("Location: pessoaForm.php?retorno1");
+    header("Location: pessoaForm.php?retorno2");
+}
 
 
 
