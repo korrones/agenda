ca2687015f43d36182a8b4aede65dd8c5dcb8b21
karol <102107721+korrones@users.noreply.github.com>
2022-06-08 21:56:19 -0300

    Update conexaoBanco.php

--- php/conexaoBanco.php
+++ php/conexaoBanco.php
@@ -2,7 +2,7 @@
 
 $usuario="root";
 $senha=""; //o seu pode ser root
-$local="127.0.0.1:3307"; //o seu pode ser localhost
+$local="127.0.0.1"; //o seu pode ser localhost
 $banco="agenda";
 
 $conexao=mysqli_connect($local, $usuario, $senha, $banco);
